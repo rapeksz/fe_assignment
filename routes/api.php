7fc3ca8dd2c7c22f2a6a386f5fd92b53016c2825
Rafał Szewc <rafal.szewc1@gmail.com>
2021-01-27 18:52:00 +0100

    add store endpoint to routes

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('comments', 'Api\CommentsController')->only('index');
+Route::resource('comments', 'Api\CommentsController')->only(['index', 'store']);
